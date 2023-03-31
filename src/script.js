993c4c411325bd96c6129dda22dd1cb6fdba2b4e
Marc <marc@langsman.com>
2023-03-31 16:15:03 +0100

    Adds GSAP. Mouse co-ords change camera position.

--- src/script.js
+++ src/script.js
@@ -1,6 +1,5 @@
 import * as THREE from 'three'
-import { Vector3 } from 'three'
-import { Scene } from 'three'
+import gsap from 'gsap'
 
 const group = new THREE.Group()
 const scene = new THREE.Scene()
@@ -43,8 +42,9 @@ const sizes = {
 
 const canvas = document.querySelector('canvas.webgl')
 
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 1, 100)
 camera.position.z = 3
+camera.lookAt(group.position)
 scene.add(camera)
 
 const renderer = new THREE.WebGLRenderer({
@@ -57,15 +57,49 @@ renderer.setSize(sizes.width, sizes.height)
 
 const clock = new THREE.Clock()
 
+/*
+    gsap.to(group.position,{
+        duration: 1,
+        delay: 1,
+        x: 2
+    })
+*/
+
+// Cursor
+const cursor = {
+    x: 0,
+    y: 0
+}
+
+// Detect for the mouse moving
+window.addEventListener('mousemove', (event) => {
+
+    cursor.x = event.clientX / sizes.width - 0.5
+    cursor.y = - (event.clientY / sizes.height - 0.5)
+
+    console.log(cursor.x, cursor.y)
+
+})
+
+
+
 const tick = () => {
 
+    
     const elapsedTime = clock.getElapsedTime()
 
     // Rotate the group of cubes
+    
     group.rotation.y = elapsedTime
-    group.position.y = Math.cos(elapsedTime) * 0.5
-    group.position.x = Math.sin(elapsedTime) * 0.5
-    group.position.z = Math.sin(elapsedTime) * 0.5 -1
+    //group.position.y = Math.cos(elapsedTime) * 0.5
+    //group.position.x = Math.sin(elapsedTime) * 0.5
+    //group.position.z = Math.sin(elapsedTime) * 0.5 -1
+    
+    camera.position.x = cursor.x
+    camera.position.y = cursor.y
+    //camera.position.x = Math.cos(elapsedTime) 
+    //camera.position.y = Math.sin(elapsedTime) 
+    //camera.lookAt(group.position)
 
     // Render the scene
     renderer.render(scene, camera)
@@ -75,5 +109,7 @@ const tick = () => {
 }
 
 tick()
+
+
 
 
