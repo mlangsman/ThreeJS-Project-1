a27001c4cdbdcc4d946cffdd62a8283f011003e1
Marc <marc@langsman.com>
2023-04-03 20:38:15 +0100

    Adds OrbitControls to replace camera control

--- src/script.js
+++ src/script.js
@@ -1,33 +1,38 @@
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import gsap from 'gsap'
+
+console.log(OrbitControls)
 
 const group = new THREE.Group()
 const scene = new THREE.Scene()
 const axesHelper = new THREE.AxesHelper(2)
 //scene.add(axesHelper)
 
+// Create objects
+
 const cube1 = new THREE.Mesh(
-    new THREE.BoxGeometry(1,1,1),
+    new THREE.BoxGeometry(1, 1, 1),
     new THREE.MeshBasicMaterial({ color: 0xff0000 })
 )
-cube1.position.set(-1.5,0,0)
+cube1.position.set(-1.5, 0, 0)
 group.add(cube1)
 
 const cube2 = new THREE.Mesh(
-    new THREE.BoxGeometry(1,1,1),
+    new THREE.BoxGeometry(1, 1, 1),
     new THREE.MeshBasicMaterial({ color: 0x00ff00 })
 )
-cube2.position.set(1.5,0,0)
+cube2.position.set(1.5, 0, 0)
 group.add(cube2)
 
 const cube3 = new THREE.Mesh(
-    new THREE.BoxGeometry(0.5,1,1),
+    new THREE.BoxGeometry(0.5, 1, 1),
     new THREE.MeshBasicMaterial({ color: 0x0000ff })
 )
-cube3.position.set(0,0,0)
+cube3.position.set(0, 0, 0)
 group.add(cube3)
 
-group.scale.set(0.5,1.5,1)
+group.scale.set(0.5, 1.5, 1)
 group.rotateZ(Math.PI * 0.25)
 group.rotateY(Math.PI * 1)
 group.rotateX(Math.PI * 0.25)
@@ -45,13 +50,16 @@ const canvas = document.querySelector('canvas.webgl')
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 1, 100)
 camera.position.z = 3
 camera.lookAt(group.position)
+
 scene.add(camera)
 
-const renderer = new THREE.WebGLRenderer({
-    canvas: canvas,
-})
+// Controls
+const controls = new OrbitControls(camera, canvas)
 
-//camera.lookAt(new THREE.Vector3(0,-1,0))
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
 
 renderer.setSize(sizes.width, sizes.height)
 
@@ -82,24 +90,17 @@ window.addEventListener('mousemove', (event) => {
 })
 
 
-
 const tick = () => {
 
-    
+
     const elapsedTime = clock.getElapsedTime()
 
     // Rotate the group of cubes
-    
-    group.rotation.y = elapsedTime
+
+    //group.rotation.y = elapsedTime * 0.1
     //group.position.y = Math.cos(elapsedTime) * 0.5
     //group.position.x = Math.sin(elapsedTime) * 0.5
     //group.position.z = Math.sin(elapsedTime) * 0.5 -1
-    
-    camera.position.x = cursor.x
-    camera.position.y = cursor.y
-    //camera.position.x = Math.cos(elapsedTime) 
-    //camera.position.y = Math.sin(elapsedTime) 
-    //camera.lookAt(group.position)
 
     // Render the scene
     renderer.render(scene, camera)
